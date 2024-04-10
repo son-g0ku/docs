7340b19361db8336fd8d9f84e95dfd4d363f1081
Efu <o@efu.me>
2024-04-10 19:55:37 +0800

    update

---
+++ components/content/UserLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <UPageGrid>
+    <UPageCard v-for="(item, index) in list" :to="item.link" :key="index" v-bind="item" target="_blank">
+      <template #icon>
+        <UAvatar
+          :src="item.src"
+          :alt="item.name"
+          class="mr-3.5"
+        />
+        <span class="w-full text-xl font-bold">{{ item.name }}</span>
+      </template>
+      <template #description>
+        <span class="line-clamp-2">{{ item.desc }}</span>
+      </template>
+    </UPageCard>
+  </UPageGrid>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  list: {
+    type: Array as () => Array<{ name: string, src: string, link: string, desc: string }>,
+    default: () => []
+  },
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,8 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
     '@nuxthq/studio',
     'nuxt-og-image',
-    '@nuxtjs/sitemap'
+    '@nuxtjs/sitemap',
+    "@nuxt/image"
   ],
   hooks: {
     'components:extend': (components) => {
